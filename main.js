adf0635533d45c9e5a3d39477b7663a9847c3acc
tanoshimane <ocr.lastonemile@gmail.com>
2023-03-28 15:04:36 +0900

    ww

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 fetchData();
 
-async function fetchData() {
+async function sendData() {
     try {
         // REST APIのエンドポイントを指定
         const apiUrl = "https://6g4lt1qeia.execute-api.us-east-2.amazonaws.com/netlifty_api_atage";
-        const response = await fetch(apiUrl);
+
+        // 送信するJSONデータ
+        const jsonData = {
+            key1: "value1",
+            key2: "value2"
+        };
+
+        const response = await fetch(apiUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(jsonData)
+        });
+
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
+
         const data = await response.json();
         console.log(data);
-        console.log(data.body);
-        const parsedData = JSON.parse(data.body);
-        console.log(parsedData);
     } catch (error) {
         console.error("Fetch error:", error);
     }
