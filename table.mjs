c477159b9217004f575c77c8641af953f39bd4db
tanoshimane <ocr.lastonemile@gmail.com>
2023-03-29 15:13:13 +0900

    ww

--- table.mjs
+++ table.mjs
@@ -1,18 +1,56 @@
+/*
 export const tableCreate = async (arr) => {
     
     createTableFromArray(arr)
 }
+*/
+export const tableCreate = async (week,kintoneRecord,dateKey,colTexts) => {
+    //ヘッダー作成
+    let headerTexts = week;
+    headerTexts.md.unshift(" ");
+    headerTexts.ymd.unshift(" ");
+    const thead = document.getElementById('my-thead');
+    const headerRow = document.createElement('tr');
+    for (let i = 0; i < headerTexts.md.length; i++) {
+      const headerCell = document.createElement('th');
+      headerCell.classList.add('col-header');
+      headerCell.textContent = headerTexts.md[i];
+      headerRow.appendChild(headerCell);
+    }
+    thead.appendChild(headerRow);
 
-function createTableFromArray(array) {
-    const table = document.getElementById("my-tbody");
-    for (let i = 0; i < array.length; i++) {
-        const row = document.createElement("tr");
-        for (let j = 0; j < array[i].length; j++) {
-            const cell = document.createElement("td");
-            const cellText = document.createTextNode(array[i][j]);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
+    //テーブル本文
+    let table = [];
+    //let kintoneRecord = await run("kintone_get","creation-takasaki",55,"wOW45LLBe9Om99ovfaGqdizguYYKYvNEpF73RMhf");
+    //let dateKey = await run("dateKey",kintoneRecord);
+    //const colTexts = await run("createHourArray",9, 18, 30);
+    for (let i = 0; i < colTexts.length; i++) {
+      table.push([colTexts[i]])
+      for (let j = 1; j < 8; j++) {
+        let value = headerTexts.ymd[j] + colTexts[i];
+        if(dateKey.indexOf(value) != -1){
+          table[i].push("×");
+        }else{
+          table[i].push("〇");
         }
-        table.appendChild(row);
+        
+      }
+    }
+    const tbody = document.getElementById("my-tbody");
+    for (let i = 0; i < table.length; i++) {
+      const row = document.createElement("tr");
+      for (let j = 0; j < table[i].length; j++) {
+        const cell = document.createElement("td");
+        if(table[i][j] == "〇"){
+          let link = document.createElement("a");
+          link.href = "https://script.google.com/macros/s/AKfycbzQEFaf7B_xTbaeurPCtDtbQPGaFZIrz3mqdEvBQIJ1xWc6vpeAxcWej_bA50m4SCmf/exec";
+          link.textContent = table[i][j]
+          cell.appendChild(link);
+        }else{
+          cell.textContent = table[i][j]
+        }
+        row.appendChild(cell);
+      }
+      tbody.appendChild(row);
     }
 }
