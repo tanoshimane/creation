66d253ee129b64415f4ece863d6a6457458762e9
tanoshimane <ocr.lastonemile@gmail.com>
2023-03-29 13:18:27 +0900

    ww

--- main.mjs
+++ main.mjs
@@ -1,7 +1,10 @@
 import * as awsApi from './awsApi.mjs';
 import * as table from './table.mjs';
+import dayjs from 'dayjs'
 main();
 async function main(){
+    const now = dayjs();
+    console.log(now.format("YYYY/MM/DD HH:mm:ss"));
     const respAws = await awsApi.awsPost();
     console.log(respAws);
     const respTablecreate = await table.tableCreate();
