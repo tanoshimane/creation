5bec3672f6ab804b293376f7ae28260ec3f43cce
tanoshimane <ocr.lastonemile@gmail.com>
2023-03-29 18:25:59 +0900

    ww

---
+++ form.mjs
@@ -0,0 +1,7 @@
+import * as awsApi from './awsApi.mjs';
+main();
+async function main(){
+    const awsBody = {code:"form"};
+    const respAws = await awsApi.awsPost(awsBody);
+    console.log(respAws);
+}
--- main.mjs
+++ main.mjs
@@ -2,7 +2,8 @@ import * as awsApi from './awsApi.mjs';
 import * as table from './table.mjs';
 main();
 async function main(){
-    const respAws = await awsApi.awsPost();
+    const awsBody = {code:"main"};
+    const respAws = await awsApi.awsPost(awsBody);
     console.log(respAws);
     const respTablecreate = await table.tableCreate(respAws.week,respAws.kintoneRecord,respAws.dateKey,respAws.colTexts);
 }
