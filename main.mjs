67334a8f8e0bac632ec2251d0d228d8f1489f5a9
tanoshimane <ocr.lastonemile@gmail.com>
2023-07-06 14:05:48 +0900

    ww

--- main.mjs
+++ main.mjs
@@ -1,8 +1,8 @@
 import * as awsApi from './awsApi.mjs';
 import * as table from './table.mjs';
 main();
-async function main(){
-    let today = dayjs().format('YYYY-MM-DD');
+async function main(targetDay){
+    targetDay = !targetDayday ? targetDay : dayjs().format('YYYY-MM-DD');
     document.getElementById('date-input').value = today;
     const awsBody = {code:"main"};
     const respAws = await awsApi.awsPost(awsBody);
@@ -10,17 +10,17 @@ async function main(){
     const respTablecreate = await table.tableCreate(respAws.week,respAws.kintoneRecord,respAws.dateKey,respAws.colTexts);
 }
 
-document.getElementById('left-arrow').addEventListener('click', function() {
+document.getElementById('left-arrow').addEventListener('click', async function() {
     console.log('Left arrow clicked!');
     // ここで何らかの処理を行う
 });
 
-document.getElementById('right-arrow').addEventListener('click', function() {
+document.getElementById('right-arrow').addEventListener('click', async function() {
     console.log('Right arrow clicked!');
     // ここで何らかの処理を行う
 });
 
-document.getElementById('date-input').addEventListener('change', function() {
+document.getElementById('date-input').addEventListener('change', async function() {
     console.log('Date input changed to ' + this.value);
-    // ここで何らかの処理を行う
+    const respTablecreate = await table.tableCreate(respAws.week,respAws.kintoneRecord,respAws.dateKey,respAws.colTexts);
 });
