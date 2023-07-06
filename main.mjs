205c377d8ce06271a3d234d298c4b48f0c3d7a53
tanoshimane <ocr.lastonemile@gmail.com>
2023-07-06 14:13:35 +0900

    ww

--- main.mjs
+++ main.mjs
@@ -3,6 +3,7 @@ import * as table from './table.mjs';
 main();
 async function main(targetDay){
     targetDay = !targetDay ? targetDay : dayjs().format('YYYY-MM-DD');
+    console.log(targetDay);
     document.getElementById('date-input').value = targetDay;
     const awsBody = {code:"main"};
     const respAws = await awsApi.awsPost(awsBody);
