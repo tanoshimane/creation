d01e1ffc80d5d1b4e73bc27b8c6bc58cff9b6a9b
tanoshimane <ocr.lastonemile@gmail.com>
2023-07-06 14:10:18 +0900

    ww

--- awsApi.mjs
+++ awsApi.mjs
@@ -2,7 +2,7 @@ export const awsPost = async (body) => {
     try {
         const apiUrl = "https://6g4lt1qeia.execute-api.us-east-2.amazonaws.com/netlifty_api_atage";
         if(!body){
-            body.code = "";
+            body.code = "main";
         }
         const response = await fetch(apiUrl, {
             method: "POST",
