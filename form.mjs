2705c3f6f162f0e4680fafe4b790dd9c4bbe4e4a
tanoshimane <ocr.lastonemile@gmail.com>
2023-07-07 08:59:16 +0900

    ww

--- form.mjs
+++ form.mjs
@@ -4,4 +4,5 @@ async function main(){
     const awsBody = {code:"form"};
     const respAws = await awsApi.awsPost(awsBody);
     console.log(respAws);
+    document.getElementById('reservDate').value = "test";
 }
